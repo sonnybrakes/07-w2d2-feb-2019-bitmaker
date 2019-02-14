8eed8e1a73862c7516dd05e3d96db6e6636026f2
Michel Frechette <mifrec@hotmail.com>
2019-02-14 17:39:00 -0500

    remove the default item='bacon'

--- bill.py
+++ bill.py
@@ -1,6 +1,6 @@
 # B is for Bacon Bill
 
-def print_bill(item='bacon'): # defining a function named print_bill()
+def print_bill(): # defining a function named print_bill()
     print()
     print("B is for {}".format(item)) # built-in print() function
     print("--------------")
