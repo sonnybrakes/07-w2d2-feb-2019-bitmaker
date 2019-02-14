d26c719e5a52669d4bb20553c9f24a167f13cbdc
Michel Frechette <mifrec@hotmail.com>
2019-02-14 18:18:40 -0500

    move some of the code into the print_header() function, and move some of the code into the print_footer() function, remove the 'pass' keyword, call the print_header() function inside the print_bill() function

--- bill.py
+++ bill.py
@@ -1,21 +1,24 @@
 # B is for Bacon Bill
 
 def print_header():
-    pass
-def print_item(item):
-    pass
-def print_footer():
-    pass
-
-def print_bill(item): # defining a function named print_bill()
     print()
-    print("B is for {}".format(item)) # built-in print() function
+    print("B is for Bacon") # built-in print() function
+    print()
     print("--------------")
+
+def print_item(item):
     print("$2.00 {}".format(item))
+
+def print_footer():
+    print_header()
     print("--------------")
     print()
     print("Please Come Again!")
     print()
-    print()
+
+def print_bill(item): # defining a function named print_bill()
+    print_header()
+    print_item(item)
+    print_footer()
 
 print_bill('bacon')
