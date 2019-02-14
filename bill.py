a0ce3f0f519e9f14b0f3e249e88009397e4939c8
Michel Frechette <mifrec@hotmail.com>
2019-02-14 16:04:56 -0500

    set default item='bacon' meaning 'bacon' is the default item unless an alternate item is passed as an argument

--- bill.py
+++ bill.py
@@ -1,15 +1,16 @@
 # B is for Bacon Bill
 
-def print_bill(item): # defining a function named print_bill()
+def print_bill(item='bacon'): # defining a function named print_bill()
     print()
     print("B is for {}".format(item)) # built-in print() function
-    print()
     print("--------------")
     print("2.00 {}".format(item))
     print("--------------")
     print()
     print("Please Come Again!")
     print()
+    print()
 
+print_bill()
 print_bill('veggie bacon')
-print_bill('bacon')
+print_bill()
