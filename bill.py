e1cbfd90b9275f41de61e6f0c4ccab9b4d9215e8
Michel Frechette <mifrec@hotmail.com>
2019-02-14 17:27:29 -0500

    add a 2nd argument of 'price', keep default item='bacon' meaning 'bacon' is the default item unless an alternate item is passed as an argument

--- bill.py
+++ bill.py
@@ -1,16 +1,15 @@
 # B is for Bacon Bill
 
-def print_bill(item='bacon'): # defining a function named print_bill()
+def print_bill(price, item='bacon'): # defining a function named print_bill()
     print()
     print("B is for {}".format(item)) # built-in print() function
     print("--------------")
-    print("2.00 {}".format(item))
+    print("${} {}".format(price, item))
     print("--------------")
     print()
     print("Please Come Again!")
     print()
     print()
 
-print_bill()
-print_bill('veggie bacon')
-print_bill()
+print_bill('4.00')
+print_bill('5.00', 'veggie bacon')
