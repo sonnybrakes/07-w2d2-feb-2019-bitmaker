53605ebce0b1a35dee111987c714c693823a6736
Michel Frechette <mifrec@hotmail.com>
2019-02-14 14:44:50 -0500

    this is a mockup of the out we are looking for using the builtin print() function

--- bill.py
+++ bill.py
@@ -1,6 +1,14 @@
 # B is for Bacon Bill
 
 def print_bill(): # defining a function named print_bill()
+    print()
     print("B is for Bacon") # built-in print() function
+    print()
+    print("--------------")
+    print("2.00 Bacon")
+    print("--------------")
+    print()
+    print("Please Come Again!")
+    print()
 
 print_bill()
