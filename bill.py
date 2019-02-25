308a113b5e70da514462232b9a5e56a3a4d8358b
Michel Frechette <mifrec@hotmail.com>
2019-02-25 07:46:50 -0500

    moved def print_bill(item): to the top of the program

--- bill.py
+++ bill.py
@@ -1,4 +1,8 @@
 # B is for Bacon Bill
+def print_bill(item): # defining a function named print_bill()
+    print_header()
+    print_item(item)
+    print_footer()
 
 def print_header():
     print()
@@ -10,15 +14,9 @@ def print_item(item):
     print("$2.00 {}".format(item))
 
 def print_footer():
-    print_header()
     print("--------------")
     print()
     print("Please Come Again!")
     print()
-
-def print_bill(item): # defining a function named print_bill()
-    print_header()
-    print_item(item)
-    print_footer()
 
 print_bill('bacon')
